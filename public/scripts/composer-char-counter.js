85712e06f985e2ddaa36d7c737e582e7f5fecfbe
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-02-19 23:28:40 +0000

    updated character counter to count backwards, added main tweet section to index.html and general styles to it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,6 @@
 $(function() {
 
-    // $('.tweet-form').on('keypress', function(){
-    //     let textCounter = $(this);
-
-    //     if (textCounter < 25) {
-    //         $(this).find('.counter').css('color', 'darkred');
-    //         //alt -- add new .class to counter to change colour
-    //     }
-
-    //     if (textCounter <= 0) {
-    //         $('.tweet-form textarea').attr('disabled', true);
-    //     } else {
-    //         textCounter -= 1;
-    //         $(this).find('.counter').html(textCounter);
-    //     }
-    // });
-
-        $('textarea').on('input', function(){
+    $('textarea').on('input', function(){
         let text = $(this);
         let counter = $(this).parent('form').find('.counter');
         let length = text.val().length;
@@ -25,16 +9,8 @@ $(function() {
         if (countdown < 50) {
             counter.css('color', 'darkred');
         } 
-        counter.html(countdown)
-        
+        counter.html(countdown);
     });
 
-
     
- 
-
-
-
-
-
 });
